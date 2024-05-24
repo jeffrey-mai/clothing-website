2da96d5e22b52168756517b875c90c51dd826cc6
Final4Head <jeffreymai3000@gmail.com>
2024-05-24 15:43:06 -0400

    Setting up the sign in page

--- client/src/components/AccountPage.jsx
+++ client/src/components/AccountPage.jsx
@@ -14,26 +14,56 @@ const AccountPage = () => {
   }
   
   const handleSignOut = () => {
-    setIsAuthenticated(false);
     Cookies.remove('authenticated');
     googleLogout();
+    setIsAuthenticated(false);
     window.location.href = '/';
   };
+
+  useEffect(() => {
+    if(!isAuthenticated){
+      const passwordInput = document.getElementById('password');
+      const togglePassword = document.getElementById('togglePassword');
+
+      togglePassword.addEventListener('change', function () {
+        if (this.checked) passwordInput.type = 'text';
+        else passwordInput.type = 'password';
+      });
+    }
+  }, [])
   
   return (
     <>
     {!isAuthenticated ? (
-      <div className='account_login'>
-          <GoogleLogin
-            onSuccess={handleSignIn}
-            onError={() => console.log('Login Failed')}
-          />
+      <div className='account_box'>
+        <div className='account_login'>
+          <h1>Login</h1>
+          <div className='account_user_pw'>
+            <form className='account_user'>
+              <label htmlFor='username'>Username</label>
+              <input type='text' id='username' name='username' placeholder='Enter your username'/>
+            </form>
+            <form className='account_pw'>
+              <label for='password'>Password</label>
+              <input type='password' id='password' name='password' placeholder='Enter your password' />
+              <input type='checkbox' id='togglePassword' />
+            </form>
+            <button className='cart_order_button'>Login</button>
+          </div>
+          <div className='account_3rd_party'>
+            <p>Or</p>
+            <GoogleLogin
+              onSuccess={handleSignIn}
+              onError={() => console.log('Login Failed')}
+            />
+          </div>
+        </div>
       </div>
     ) : (
       <div className='account_summary'>
-          <NavBar />
-          <button onClick={handleSignOut}>Sign Out</button>
-          <ToS />
+        <NavBar />
+        <button onClick={handleSignOut}>Sign Out</button>
+        <ToS />
       </div>
     )}
     </>
